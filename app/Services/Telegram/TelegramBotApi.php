5700563b55eece54f075370fea258f5d37c31319
Alex Shestakov <public_mce@rambler.ru>
2023-02-06 21:25:08 +0300

    Custom Exception #WIP

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace App\Services\Telegram;
 
+use App\Exceptions\TelegramException;
 use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
 
 class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chat_id, string $message): void
+    public static function sendMessage(string $token, int $chat_id, string $message): bool
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chat_id,
-            'text'    => $message
-        ]);
+        try {
+            $response = Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chat_id,
+                'text'    => $message
+            ]);
+            if ($response->status() === Response::HTTP_OK){
+                return true;
+            }
+
+            return false;
+        } catch (TelegramException $exception) {
+
+        }
+        return false;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    logger()->channel('telegram')->debug('$message');
     return view('welcome');
 });
