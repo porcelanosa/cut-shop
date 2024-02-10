41df0724cfcfdcee355f8f188e75bc4ea3bd8285
Alex Shestakov <public_mce@rambler.ru>
2024-02-10 21:07:08 +0300

    Auth start

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.auth')
 
-        <title>Laravel</title>
-
-        @vite(['resources/css/app.css', 'resources/sass/main.sass', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased">
-        <h1>Cut Shop</h1>
-    </body>
-</html>
+@section('content')
+    <h1>Cut Shop</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,9 @@ Route::get('/', function () {
 //        logger()->channel('telegram')->debug('$message');
 
     return view('welcome');
-});
+})->name('home');
+
+
+Route::get('/login', function () {
+    return view('auth.index');
+})->name('login');
